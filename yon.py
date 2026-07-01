1737c9f4480cc303dfb630a2153f1745b6e67335
horiesotaro <sougoro1947@gmail.com>
2026-07-01 15:45:13 +0900

    Update yon.py

--- yon.py
+++ yon.py
@@ -25,82 +25,82 @@ with st.form("input_form"):
 
     sex = st.selectbox('マッチしたい性別を選択', ['男', '女','両方'])
     day = st.selectbox('希望日を選択', ['6/7', '6/8','6/9'])
-    time = st.selectbox('希望時間を選択', ['昼', '夜','一日'])
+    time = st.selectbox('希望時間を選択', ['昼', '夜'])
     mbti = st.selectbox('MBTIを選択', [
         "ISFP","ISFJ","ISTP","ISTJ","INFP","INFJ","INTP","INTJ",
         "ESFP","ESFJ","ESTP","ESTJ","ENFP","ENFJ","ENTP","ENTJ"
     ])
-    tension=st.selectbox('1:似たテンションの人とマッチしたい割合',range(0,11))
-    idea=st.selectbox('2:似た考えの人とマッチしたい割合',range(0,11))
-    judge=st.selectbox('3:感情的な人とマッチしたい割合',range(0,11))
-    plan=st.selectbox('4:一緒に計画を立てたいしたい割合',range(0,11))
+    st.text('\n以下は1～5を選択してください')
+    st.text('1:全くそう思わない 2:そう思わない　3:どちらでもない 4:そう思う 5:とてもそう思う')
+    tension=st.selectbox('1:同じようなテンション感の人と遊びたい',range(1,6))
+    idea=st.selectbox('2:似た考え方の人と遊びたい',range(1,6))
+    judge=st.selectbox('3:協調性が高い人or思考力が高い人と遊びたい',range(1,6))
+    st.text('1:協調性が高い人 2:やや協調性寄り　3:どちらでもない 4:やや思考力寄り 5:思考力が高い人')
+    plan=st.selectbox('4:前々からみんなでしっかり計画を練りたいorその場の流れで遊びたい',range(1,6))
+    st.text('1:綿密な計画を決めておきたい 2:やや1寄り　3:行く場所くらいは決めたい 4:やや5寄り 5:時間と集合場所だけでOK')
     submitted = st.form_submit_button("保存")
 
 if submitted:
     st.write("保存完了！")
         
-max1=('一日')
 a = mbti[0:4]
 
 
 def orientation(a,tension,idea,judge,plan):
     if a[0]=='E':
         e=int(tension)
-        i=10-int(tension)
+        i=6-int(tension)
     else:
         i=int(tension)
-        e=10-int(tension)
+        e=6-int(tension)
     if a[1]=='N':
         n=int(idea)
-        s=10-int(idea)
+        s=6-int(idea)
     else:
         s=int(idea)
-        n=10-int(idea)
+        n=6-int(idea)
         
-    f=int(judge)
-    t=10-int(judge)
-    j=int(plan)
-    p=10-int(plan)
-    return e,i,n,s,f,t,j,p
-
+    t=int(judge)
+    f=6-int(judge)
+    p=int(plan)
+    j=6-int(plan)
+    return e,i,n,s,t,f,p,j
 
 # In[85]:
 
 
-result0_df=(mbti_df[(mbti_df['性別'] == sex)&(mbti_df['希望日'] == day)&((mbti_df['希望時間'] == time)|(mbti_df['希望時間'] == max1))])
-result1_df=(mbti_df[(mbti_df['希望日'] == day)&((mbti_df['希望時間'] == time)|(mbti_df['希望時間'] == max1))])#性別両方
-result2_df=(mbti_df[(mbti_df['性別'] == sex)&(mbti_df['希望日'] == day)]) #希望時間一日
-result3_df=(mbti_df[(mbti_df['希望日'] == day)]) #性別両方かつ希望時間一日
-#ここにif文を入れる
-if (sex=='両方')&(time=='一日'):
-    y_df=result3_df
-elif (time=='一日'):
-    y_df=result2_df
-elif (sex=='両方'):
+result0_df=(mbti_df[(mbti_df['性別'] == sex)&(mbti_df['希望日'] == day)&((mbti_df['希望時間'] == time))])
+result1_df=(mbti_df[(mbti_df['希望日'] == day)&((mbti_df['希望時間'] == time))])
+
+if (sex=='両方'):
     y_df=result1_df
 else:
     y_df=result0_df
 d_df=y_df.copy()
 
+# 各MBTIの人数をカウントする
+mbti_counts = d_df['MBTI'].value_counts()
 
 
-def scoring(e,i,n,s,f,t,j,p):
-    ISFP=i+s+f+p
-    ISFJ=i+s+f+j
-    ISTP=i+s+t+p
-    ISTJ=i+s+t+j
-    INFP=i+n+f+p
-    INFJ=i+n+f+j
-    INTP=i+n+t+p
-    INTJ=i+n+t+j
-    ESFP=e+s+f+p
-    ESFJ=e+s+f+j
-    ESTP=e+s+t+p
-    ESTJ=e+s+t+j
-    ENFP=e+n+f+p
-    ENFJ=e+n+f+j
-    ENTP=e+n+t+p
-    ENTJ=e+n+t+j
+
+
+def scoring(e,i,n,s,t,f,p,j):
+    ISFP=(i+s+f+p)*2
+    ISFJ=(i+s+f+j)*2
+    ISTP=(i+s+t+p)*2
+    ISTJ=(i+s+t+j)*2
+    INFP=(i+n+f+p)*2
+    INFJ=(i+n+f+j)*2
+    INTP=(i+n+t+p)*2
+    INTJ=(i+n+t+j)*2
+    ESFP=(e+s+f+p)*2
+    ESFJ=(e+s+f+j)*2
+    ESTP=(e+s+t+p)*2
+    ESTJ=(e+s+t+j)*2
+    ENFP=(e+n+f+p)*2
+    ENFJ=(e+n+f+j)*2
+    ENTP=(e+n+t+p)*2
+    ENTJ=(e+n+t+j)*2
     return (ISFP,ISFJ,ISTP,ISTJ,INFP,INFJ,INTP,INTJ,ESFP,ESFJ,ESTP,ESTJ,ENFP,ENFJ,ENTP,ENTJ)
 
 
@@ -110,14 +110,49 @@ def scoring(e,i,n,s,f,t,j,p):
 x_df=["ISFP","ISFJ","ISTP","ISTJ","INFP","INFJ","INTP","INTJ","ESFP","ESFJ","ESTP","ESTJ","ENFP","ENFJ","ENTP","ENTJ"]
 w_df=pd.DataFrame(x_df, columns = ["MBTI"])
 # e_df=(ISFP,ISFJ,ISTP,ISTJ,INFP,INFJ,INTP,INTJ,ESFP,ESFJ,ESTP,ESTJ,ENFP,ENFJ,ENTP,ENTJ)
-(e,i,n,s,f,t,j,p)=orientation(a,tension,idea,judge,plan)
-e_df = scoring(e,i,n,s,f,t,j,p)
+(e,i,n,s,t,f,p,j)=orientation(a,tension,idea,judge,plan)
+e_df = scoring(e,i,n,s,t,f,p,j)
+aaa=scoring(e,i,n,s,t,f,p,j)
 c_df=pd.DataFrame(e_df, columns = ["相手への評価"])
 q_df=pd.concat([w_df,c_df],axis=1)
+datacd=[]
+for i in range(16):
+    if aaa[i]>=24:
+        datacd.append(x_df[i])
+impo=len(datacd)
+
+# 1. まずd_dfからMBTIの人数を集計してデータフレームにする
+mbti_counts = d_df['MBTI'].value_counts().reset_index()
+mbti_counts.columns = ['MBTI', '人数']
+
+# 2. 集計結果と q_df を結合する
+summary_df = pd.merge(q_df, mbti_counts, on='MBTI', how='left')
 
 
+ #1. 相手への評価が24以上のMBTIのリストを取得する
+if max(aaa)>=28:
+    target_mbti = q_df[q_df['相手への評価'] >= 28]['MBTI'].tolist()
+else:
+    target_mbti = q_df[q_df['相手への評価'] >= (max(aaa))]['MBTI'].tolist()
 
-# In[89]:
+# 2. d_dfの中で、そのMBTIリストに含まれるユーザー（行）の数をカウントする
+total_users = d_df['MBTI'].isin(target_mbti).sum()
+
+print(f"相手への評価が28or MAX以上のMBTIに属するユーザーの合計人数: {total_users}人")
+
+a=(total_users)/(len(d_df))
+
+if a>=0.86:
+    bbq=32
+elif (a>=0.71)and(a<0.86):
+    bbq=30
+elif (a>=0.56)and(a<0.71):
+    bbq=28
+elif (a>=0.41)and(a<0.56):
+    bbq=26
+else:
+    bbq=24
+print(bbq) 
 
 
 y_df['相手への評価'] = 0
@@ -126,7 +161,7 @@ for i in range(y_df.shape[0]):
     mbti2 = y_df.iloc[i,3]
     y_df.iloc[i,8]=q_df[q_df['MBTI'] == mbti2].loc[:,'相手への評価'].values[0]
 s_df=y_df.copy()
-
+s_df
 
 
 # In[90]:
@@ -151,9 +186,9 @@ for i in range(1,len(s_df.index)):
 # In[91]:
 
 
-g_df=s_df.iloc[0:,8:]
-
-
+w_df=s_df.iloc[0:,8:]
+w_df.columns=w_df.index
+kids_df=w_df
 # In[92]:
 
 
@@ -165,26 +200,64 @@ g_df=s_df.iloc[0:,8:]
 import numpy as np
 import pandas as pd
 
-df = g_df.copy()
+df = w_df.copy()
 
 # 空のマスク（全部False）
 mask = pd.DataFrame(False, index=df.index, columns=df.columns)
+
 
 n = len(df)
 
 for x in range(n):
     for y in range(n):
-        if x!=y:
+        if x != y:
             a = df.iloc[x, y]
             b = df.iloc[y, x]
-            if (a >= 15 and b >= 15 ) and (a >= 25 or b >= 25):
-                mask.iloc[x, y] = True
-                mask.iloc[y, x] = True
 
-# 条件を満たす部分だけ残す（他はNaN）
-kid_df = df.where(mask,0)
+            if(max(aaa)>=28):
+                # 1行目（x=0）または1列目（y=0）が絡む場合
+                if y == 0:
+                # 一方が24以上、もう一方が（bbq）以上なら残す
+                    if (a >= 28 and b >= bbq):
+                        mask.iloc[x, y] = True
+                        mask.iloc[y, x] = True
+                elif x == 0:
+                    if (b >= 28 and a >= bbq):
+                        mask.iloc[x, y] = True
+                        mask.iloc[y, x] = True
+                
+            
+            # 1行目、1列目以外の一般のデータの場合
+                else:
+                # お互いに28以上（28以上で統一）なら残す
+                    if max(a,b)>=28 and min(a,b)>=24:
+                        mask.iloc[x, y] = True
+                        mask.iloc[y, x] = True
+                        
+                        
+            if(max(aaa)<28):
+                if y == 0:
+                # 一方が24以上、もう一方が（bbq）以上なら残す
+                    if (a >= max(aaa) and b >= bbq):
+                        mask.iloc[x, y] = True
+                        mask.iloc[y, x] = True
+                elif x == 0:
+                    if (b >= max(aaa) and a >= bbq):
+                        mask.iloc[x, y] = True
+                        mask.iloc[y, x] = True
+                
+            
+            # 1行目、1列目以外の一般のデータの場合
+                else:
+                # お互いに28以上（28以上で統一）なら残す
+                    if max(a,b)>=28 and min(a,b)>=24:
+                        mask.iloc[x, y] = True
+                        mask.iloc[y, x] = True
+                            
+                            
 
-
+# 条件を満たす部分だけ残す
+kid_df = df.where(mask, 0)
 
 # In[94]:
 
@@ -198,8 +271,35 @@ kid_df = df.where(mask,0)
 
 # In[95]:
 
+# 1. 元のデータフレームをコピーしてベースを作成
+new_df = kid_df.copy()
 
-new_df=kid_df.where(kid_df>=25,"x")
+# 2. 条件分岐
+if max(aaa) < 28:
+    # --- max(aaa)が28未満の時 ---
+    
+    # 1行目 (indexが0) または 1列目 (columnsの最初) の条件マスク
+    # ※お使いのデータのインデックス名/カラム名に合わせて指定してください
+    row0_mask = new_df.index == new_df.index[0]
+    col0_mask = new_df.columns == new_df.columns[0]
+    is_first_row_or_col = row0_mask[:, None] | col0_mask[None, :]
+    
+    # 条件A: 1行目・1列目で、値が max(aaa) 未満
+    mask_a = is_first_row_or_col & (new_df < max(aaa))
+    
+    # 条件B: それ以外（1行目・1列目ではない）で、値が 28 未満
+    mask_b = ~is_first_row_or_col & (new_df < 28)
+    
+    # 両方の条件を結合し、当てはまる場所を "x" に置換
+    final_mask = mask_a | mask_b
+    new_df = new_df.mask(final_mask, "x")
+
+else:
+    # --- max(aaa)が28以上の時 ---
+    # 全ての行列で28未満の数字に "x"
+    new_df = new_df.mask(new_df < 28, "x")
+
+# 結果の確認
 
 
 
@@ -225,7 +325,7 @@ for j in range(len(new_df.index)):
 # In[99]:
 
 
-user_id = g_df.index
+user_id = w_df.index
 
 
 # In[100]:
@@ -267,7 +367,7 @@ for c in cliques:
                 result.append(list(sorted(sub)))
 
 id2pos = {}
-for pos, id, in enumerate(g_df.index):
+for pos, id, in enumerate(w_df.index):
     id2pos[id] = pos
 print(id2pos)
 
@@ -278,7 +378,7 @@ datar=[]
 for g in result:
     for i,j in product(range(4),range(4)):
         if i != j:
-            dataq.append(g_df.iat[id2pos[g[i]],id2pos[g[j]]])
+            dataq.append(w_df.iat[id2pos[g[i]],id2pos[g[j]]])
 n=12
 for c in range(0,len(dataq),n):
     datar.append(dataq[c:c+n])
